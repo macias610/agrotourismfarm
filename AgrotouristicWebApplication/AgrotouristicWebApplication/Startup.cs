edb8b5121c91f50f07d98fff33d52b4e7cf698ce
Maciej Pluta <maciek1021@poczta.onet.pl>
2017-07-17 00:06:19 +0200

    Utworzono pusty projekt aplikacji webowej ASP.NET MVC dla gospodarstwa agroturystycznego.

---
+++ AgrotouristicWebApplication/AgrotouristicWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgrotouristicWebApplication.Startup))]
+namespace AgrotouristicWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
